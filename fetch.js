b038973673d5054e9e09b54d0522a80154b5ae1f
Dan Kaplun <dbkaplun@gmail.com>
2015-11-11 21:30:19 -0500

    wip feat(images): show first Google Image Search result

--- fetch.js
+++ fetch.js
@@ -3,11 +3,22 @@
 var Nightmare = require('nightmare');
 var Promise = require('bluebird');
 var vo = require('vo');
+var url = require('url');
+var http = require('http');
+var request = require('request-promise');
+
+/**
+ * BEGIN HELPERS
+ */
 
 trim.surroundingWhitespaceRegex = /^\s+|\s+$/g;
 function trim (str) {
   return str.replace(trim.surroundingWhitespaceRegex, '');
 }
+
+/**
+ * BEGIN BUSINESS LOGIC
+ */
 
 var fetchAndysAutoSport = Promise.promisify(vo(function* () {
   var nightmare = Nightmare();
@@ -70,8 +81,30 @@ function fetchWheelDatabase () {
   });
 }
 
-if (require.main === module) fetchWheelDatabase()
+fetchGoogleImages.urlObj = url.parse('http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=', true);
+delete fetchGoogleImages.urlObj.search; // query obj stored in urlObj.query, unused if .search is present
+function fetchGoogleImages (data) {
+  console.log('fetchGoogleImages', data.wheels.length);
+  return Promise
+    .map(data.wheels, function (wheel) {
+      fetchGoogleImages.urlObj.query.q = wheel.name;
+      return request(url.format(fetchGoogleImages.urlObj)).then(function (results) {
+        console.log('res', results);
+        return wheel;
+      });
+    })
+    .then(function (wheels) {
+      data.wheels = wheels;
+      return data;
+    });
+}
+
+function fetch () {
+  return fetchWheelDatabase().then(fetchGoogleImages);
+}
+
+if (require.main === module) fetch()
   .then(function (wheels) { return JSON.stringify(wheels, null, '  '); })
   .then(console.log);
 
-module.exports = fetchWheelDatabase;
+module.exports = fetch;
